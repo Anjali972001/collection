55a63adf8cba91517a0d0775c83efbd6068c9ca8
Anjali <anjalikumari972001@gmail.com>
2022-04-03 22:32:03 +0530

    Initial commit

---
+++ src/arrayListLearning.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class arrayListLearning {
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        System.out.println(a);
+        a.set(1,5);
+        System.out.println(a);
+        a.remove(3);
+        System.out.println(a);
+        
+
+    }
+}
